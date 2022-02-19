a3df09c92d4466738f7cd8c63f147f2fac61ff4e
Juan E <jescobar15@hotmail.com>
2022-02-19 16:57:32 -0500

    add better version rock paper scissors

--- script.js
+++ script.js
@@ -1,57 +1,70 @@
-let playerSelection = prompt("rock paper or scissors")
 
-let computerPlay = function() {
 
-  let randomNum = Math.floor(Math.random() * 3)
+computerPlay = () => {
+  let randomNum = Math.floor(Math.random()*3)
 
-  switch (randomNum) {
-    case 0: 
+  switch(randomNum){
+    case 0:
       return "rock"
-      brake;
-    case 1: 
+      break
+    case 1:
       return "paper"
-      brake;
+      break
     case 2:
       return "scissors"
-      brake
+      break
   }
 }
 
 
-let computerResult = computerPlay()
-let playerResult = playerSelection.toLowerCase()
 
-console.log(`computer result: ${computerResult}`)
-console.log(`player result: ${playerResult}`)
 
-let startGame = function() {
-  if (playerResult === "rock" && computerResult === "rock") {
-    return console.log("tie on rock")
+playRound = () => {
+  let playerChoose = prompt("choose")
+  let playerSelection = playerChoose.toLowerCase()
+  let computerSelection = computerPlay()
 
-  } else if (playerResult === "rock" && computerResult === "paper") {
-    return console.log("paper over rock, pc win")
-
-  } else if (playerResult === "rock" && computerResult === "scissors") {
-    return console.log("rock over scissors, player win")
-
-  } else if (playerResult === "paper" && computerResult === "rock") {
-    return console.log("paper over rock, player win")
-
-  } else if (playerResult === "paper" && computerResult === "paper") {
-    return console.log("tie on paper")
-
-  } else if (playerResult === "paper" && computerResult === "scissors") {
-    return console.log("paper over scissors, pc win")
-
-  } else if (playerResult === "scissors" && computerResult === "rock") {
-    return console.log("rock over scissors, pc win")
-
-  } else if (playerResult === "scissors" && computerResult === "paper") {
-    return console.log("sscissors over paper, player win")
-
-  } else if (playerResult === "scissors" && computerResult === "scissors") {
-    return console.log("tie on scissors")
+  if (playerSelection === computerSelection) {
+    return `player: ${playerSelection} vs computer: ${computerSelection}
+    it's a tie!
+    `
+  } else if (playerSelection === "rock") {
+      if (computerSelection === "paper") {
+        return `player: ${playerSelection} vs computer: ${computerSelection}
+        you lose
+        `
+    } 
+      if (computerSelection === "scissors") {
+        return `player: ${playerSelection} vs computer: ${computerSelection}
+        you won!
+        `
+    }
+    
+  } 
+    else if (playerSelection === "paper") {
+      if (computerSelection === "rock") {
+        return `player: ${playerSelection} vs computer: ${computerSelection}
+        you won!
+      `
+    } 
+      if (computerSelection === "scissors") {
+        return `player: ${playerSelection} vs computer: ${computerSelection}
+        you lose
+      `
+    }
   }
+    else if (playerSelection === "scissors") {
+      if (computerSelection === "rock") {
+        return `player: ${playerSelection} vs computer: ${computerSelection}
+        you lose
+      `
+    } 
+      if (computerSelection === "paper") {
+        return `player: ${playerSelection} vs computer: ${computerSelection}
+        you won!
+      `
+    }
+}
 }
 
-startGame()
+console.log(playRound())
